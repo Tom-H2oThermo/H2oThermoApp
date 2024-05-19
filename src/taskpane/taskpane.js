1fba5b5fbec46c05a7fe48a38570152b620f395d
Tom-H2oThermo <Tom@H2oThermo.com>
2024-05-19 16:02:28 -0600

    Added import { PublicClientNext } from "@azure/msal-browser"; to the top of the taskpane.js file

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,6 +6,8 @@
 /* global console, document, Excel, Office */
 
 // The initialize function must be run each time a new page is loaded
+import { PublicClientNext } from "@azure/msal-browser";
+
 Office.onReady(() => {
   document.getElementById("sideload-msg").style.display = "none";
   document.getElementById("app-body").style.display = "flex";
