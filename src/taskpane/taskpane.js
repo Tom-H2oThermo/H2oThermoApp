60361dad1e5baaeb970b978ebfef4765119baeed
Tom-H2oThermo <Tom@H2oThermo.com>
2024-06-29 13:16:48 -0600

    Revert "Added the TODO 1-4 sections"
    This reverts commit cca53808dadb6310d6c2b0e439cbdd211fad09b0.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -3,7 +3,7 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global console, document, Office */
+/* global console, document, Excel, Office */
 
 // The initialize function must be run each time a new page is loaded
 import { PublicClientNext } from "@azure/msal-browser";
@@ -26,55 +26,24 @@ Office.onReady(async (info) => {
   }
 });
 
-async function run() {
-  // Specify minimum scopes needed for the access token.
-  const tokenRequest = {
-    scopes: ["Files.Read", "User.Read", "openid", "profile"],
-  };
-  let accessToken = null;
-
+export async function run() {
   try {
-    console.log("Trying to acquire token silently...");
-    const userAccount = await pca.acquireTokenSilent(tokenRequest);
-    console.log("Acquired token silently.");
-    accessToken = userAccount.accessToken;
-  } catch (error) {
-    console.log(`Unable to acquire token silently: ${error}`);
-  }
+    await Excel.run(async (context) => {
+      /**
+       * Insert your Excel code here
+       */
+      const range = context.workbook.getSelectedRange();
 
-  if (accessToken === null) {
-    // Acquire token silent failure. Send an interactive request via popup.
-    try {
-      console.log("Trying to acquire token interactively...");
-      const userAccount = await pca.acquireTokenPopup(tokenRequest);
-      console.log("Acquired token interactively.");
-      accessToken = userAccount.accessToken;
-    } catch (popupError) {
-      // Acquire token interactive failure.
-      console.log(`Unable to acquire token interactively: ${popupError}`);
-    }
-  }
+      // Read the range address
+      range.load("address");
 
-  // Log error if both silent and popup requests failed.
-  if (accessToken === null) {
-    console.error(`Unable to acquire access token.`);
-    return;
-  }
+      // Update the fill color
+      range.format.fill.color = "red";
 
-  // Call the Microsoft Graph API with the access token.
-  const response = await fetch(`https://graph.microsoft.com/v1.0/me/drive/root/children?$select=name&$top=10`, {
-    headers: { Authorization: accessToken },
-  });
-
-  if (response.ok) {
-    // Write file names to the console.
-    const data = await response.json();
-    const names = data.value.map((item) => item.name);
-    names.forEach((name) => {
-      console.log(name);
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
     });
-  } else {
-    const errorText = await response.text();
-    console.error("Microsoft Graph call failed - error text: " + errorText);
+  } catch (error) {
+    console.error(error);
   }
 }
